57952382d567264e844bb907db13759c69430ac7
DESKTOP-BETG49P\A <fishwwww0516@naver.com>
2026-01-06 14:05:51 +0900

    feat: header & footer on page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h1>Cyber Shop</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
